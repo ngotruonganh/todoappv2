8fa4529feca57d19a69c76614aad99ddb1eb4e69
Anh Ngo <ngotruonganh1901.com>
2023-06-05 23:33:51 +0700

    Sort todo

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -1,24 +1,25 @@
 import { Col, Radio, Row } from "antd";
 import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import todosSlice from "../TodoList/todosSlice";
+import { useDispatch } from "react-redux";
+import todosSlice from "../TodoList/todosSlice";
 
 export default function Sort() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  const [type, setType] = useState("az");
+  const [value, setValue] = useState("id");
 
-  const changeType = (e) => {
-    setType(e.target.value);
-    // dispatch(todosSlice.actions.sortTodo());
+  const onChange = (e) => {
+    setValue(e.target.value);
+    dispatch(todosSlice.actions.sortTodo(e.target.value));
   };
 
   return (
     <Row>
       <Col>
-        <Radio.Group value={type} onChange={changeType}>
-          <Radio value="az">A-Z</Radio>
-          <Radio value="za">Z-A</Radio>
+        <Radio.Group onChange={onChange} value={value}>
+          <Radio value={"id"}>Newest</Radio>
+          <Radio value={"az"}>A - Z</Radio>
+          <Radio value={"za"}>Z - A</Radio>
         </Radio.Group>
       </Col>
     </Row>
--- src/components/TodoList/todosSlice.js
+++ src/components/TodoList/todosSlice.js
@@ -1,26 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
+const _ = require("lodash");
 
 export default createSlice({
   name: "todoList",
-  initialState: [
-    { id: 1, name: "Thanh toan hoc phi", completed: false, priority: "Medium" },
-    { id: 2, name: "Don dep nha", completed: true, priority: "High" },
-    { id: 3, name: "Di cho", completed: false, priority: "Low" },
-    { id: 4, name: "Lam bai tap", completed: false, priority: "Medium" },
-    { id: 5, name: "Mua điện thoại mới", completed: false, priority: "High" },
-  ],
+  initialState: {
+    list: [
+      {
+        id: 1,
+        name: "Hoc phi",
+        completed: false,
+        priority: "Medium",
+      },
+      { id: 2, name: "Don dep nha", completed: true, priority: "High" },
+      { id: 3, name: "Di cho", completed: false, priority: "Low" },
+      { id: 4, name: "Lam bai tap", completed: false, priority: "Medium" },
+      { id: 5, name: "Mua điện thoại mới", completed: false, priority: "High" },
+    ],
+    sortType: "id",
+  },
   reducers: {
     addTodo: (state, action) => {
-      state.push(action.payload);
+      state.list.push(action.payload);
     },
     deleteTodo: (state, action) => {
-      const currentTodo = state.findIndex((todo) => todo.id === action.payload);
+      const currentTodo = state.list.findIndex(
+        (todo) => todo.id === action.payload
+      );
       if (currentTodo !== -1) {
-        state.splice(currentTodo, 1);
+        state.list.splice(currentTodo, 1);
+      }
+    },
+    sortTodo: (state, action) => {
+      state.sortType = action.payload;
+      var clone = _.cloneDeep(state.list);
+      if (state.sortType === "id") {
+        clone.sort((a, b) => a.id - b.id);
+        state.list = clone;
+      }
+      if (state.sortType === "az") {
+        clone = _.sortBy(clone, (o) => o.name.toLowerCase());
+        state.list = clone;
+      }
+      if (state.sortType === "za") {
+        clone = _.sortBy(clone, (o) => o.name.toLowerCase()).reverse();
+        state.list = clone;
       }
     },
     toggleTodoStatus: (state, action) => {
-      const currentTodo = state.find((todo) => todo.id === action.payload);
+      const currentTodo = state.list.find((todo) => todo.id === action.payload);
       if (currentTodo) {
         currentTodo.completed = !currentTodo.completed;
       }
